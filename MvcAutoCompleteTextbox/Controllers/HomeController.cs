70f3788a12b063cf83ac9cf3c681be1fafadf9f1
Sandy <srachel27@gmail.com>
2014-11-07 13:30:43 +1100

    initial commit

---
+++ MvcAutoCompleteTextbox/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcAutoCompleteTextbox.Models;
+
+namespace MvcAutoCompleteTextbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static SampleDBContext db = new SampleDBContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchTerm)
+        {
+            List<Student> students;
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                students = db.Students.ToList();
+            }
+            else
+            {
+                students = db.Students.Where(x => x.Name.StartsWith(searchTerm)).ToList();
+
+            }
+            return View(students);
+            
+        }
+
+
+        public JsonResult GetStudents(string term)
+        {
+            
+            List<string> students = db.Students.Where(s => s.Name.StartsWith(term))
+                .Select(x => x.Name).ToList();
+            return Json(students, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ MvcAutoCompleteTextbox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAutoCompleteTextbox.MvcApplication" Language="C#" %>
